d4e2cf4fa6a1b13b4b52e6016436d0d7c76baafa
Johnny Melo <Johnnymelowork@gmail.com>
2018-12-30 03:49:38 -0500

    updated htmlroute file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
-  app.use(function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
